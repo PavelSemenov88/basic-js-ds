1a6d0b59a7cae4fc9410ab895177f217c1b53552
PavelSemenov88 <semencheb@yandex.ru>
2023-04-15 00:44:44 +0300

    fead: remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,8 +22,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k) {
+  let prev = null;
+  let head = l;
+
+  if(l.value === k) {
+    l = l.next;
+  }
+
+  while(head.next) {
+    if(head.next.value === k) {
+      prev = head.next;
+      head.next = head.next.next;
+    } else {
+      head = head.next;
+    }
+    
+  }
+  return l;
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
